c67c7d5f1f80cc184e2c4d0e4767ec23115cd7e8
alonso salas <alonso.salas@usach.cl>
2024-01-17 19:53:52 +0000

    asdfsf

--- besu/crear_static_nodes_json.sh
+++ besu/crear_static_nodes_json.sh
@@ -1,7 +1,26 @@
-json_file="path/to/json/file.json"
-keys_folder="besu/keys/keys"
+json_file="file.json"
+keys_folder="/home/alonso.salas/Besu-Benhmark-EHR/besu/keys/keys"
+n=4
+enodes=()
 
-for folder in "$keys_folder"/node-*; do
+# Create json_file if it does not exist
+if [ ! -f "$json_file" ]; then
+    echo '{"nodes": []}' > "$json_file"
+fi
+
+for ((i=1; i<n; i++)); do
+    folder="$keys_folder/node-$i"
     public_key=$(cat "$folder/key.pub")
-    jq --argjson key "$public_key" '.nodes += [$key]' "$json_file" > "$json_file.tmp" && mv "$json_file.tmp" "$json_file"
+    enode="enode://$public_key@10.0.64.$(($i+1)):3030"
+    enodes+=("$enode")
 done
+
+tmp_file="$json_file.tmp"
+cp "$json_file" "$tmp_file"
+
+# Add enodes to the JSON file
+enodes_str=$(IFS=,; echo "${enodes[*]}")
+sed -i "s#\"nodes\": \[\]#\"nodes\": [$enodes_str]#" "$tmp_file"
+
+# Replace the original JSON file with the updated one
+mv "$tmp_file" "$json_file"
