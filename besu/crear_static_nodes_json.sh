818b20795128e9bfaf290106803c5d8de16395b1
alonso salas <alonso.salas@usach.cl>
2024-01-18 17:30:45 +0000

    tx pool layered price bump 0

--- besu/crear_static_nodes_json.sh
+++ besu/crear_static_nodes_json.sh
@@ -3,24 +3,18 @@ keys_folder="/home/alonso.salas/Besu-Benhmark-EHR/besu/keys/keys"
 n=4
 enodes=()
 
-# Create json_file if it does not exist
-if [ ! -f "$json_file" ]; then
-    echo '{"nodes": []}' > "$json_file"
-fi
-
 for ((i=1; i<n; i++)); do
     folder="$keys_folder/node-$i"
     public_key=$(cat "$folder/key.pub")
-    enode="enode://$public_key@10.0.64.$(($i+1)):3030"
+    enode="\"enode://$public_key@10.0.64.$(($i+1)):3030\""
     enodes+=("$enode")
 done
 
-tmp_file="$json_file.tmp"
-cp "$json_file" "$tmp_file"
+# Create json_file if it does not exist
+if [ ! -f "$json_file" ]; then
+    touch "$json_file"
+fi
 
 # Add enodes to the JSON file
 enodes_str=$(IFS=,; echo "${enodes[*]}")
-sed -i "s#\"nodes\": \[\]#\"nodes\": [$enodes_str]#" "$tmp_file"
-
-# Replace the original JSON file with the updated one
-mv "$tmp_file" "$json_file"
+echo "[$enodes_str]" > "$json_file"
